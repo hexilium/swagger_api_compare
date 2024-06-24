4d88cae6bb371b3b18529def363a26b16a5def2f
hexilium <hexilium@gmail.com>
2024-06-24 16:53:55 +0300

    updated paths and added comparisons saving

--- compare.py
+++ compare.py
@@ -5,7 +5,10 @@ from deepdiff import DeepDiff
 from swagger_spec_validator.validator20 import validate_spec
 from swagger_spec_validator.common import SwaggerValidationError
 from datetime import datetime
+from urllib.parse import urlparse
 
+cur_spec = 'current'
+diff = 'differences'
 
 def load_spec(file_path_or_url):
     """Загружает спецификацию из JSON файла или URL."""
@@ -13,9 +16,14 @@ def load_spec(file_path_or_url):
         if file_path_or_url.startswith('http://') or file_path_or_url.startswith('https://'):
             response = requests.get(file_path_or_url)
             response.raise_for_status()
-            spec = response.json()
+            response.encoding = 'utf-8'  # Устанавливаем кодировку
+            try:
+                spec = response.json()
+            except json.JSONDecodeError:
+                print(f"Ответ по URL {file_path_or_url} не содержит допустимый JSON.")
+                return None
         else:
-            with open(file_path_or_url, 'r') as file:
+            with open(file_path_or_url, 'r', encoding='utf-8') as file:
                 spec = json.load(file)
         # Проверка на валидность спецификации Swagger
         validate_spec(spec)
@@ -34,18 +42,33 @@ def load_spec(file_path_or_url):
         return None
 
 
-def save_spec(spec, file_path):
+def save_spec(spec, directory, base_filename):
     """Сохраняет спецификацию в JSON файл с временной меткой."""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    file_path_with_timestamp = file_path.replace('.json', f'_{timestamp}.json')
-    with open(file_path_with_timestamp, 'w') as file:
+    file_path_with_timestamp = os.path.join(directory, f"{base_filename}_{timestamp}.json")
+    with open(file_path_with_timestamp, 'w', encoding='utf-8') as file:
         json.dump(spec, file, indent=4)
+
+
+def save_diff(diff, directory, base_filename):
+    """Сохраняет результат сравнения в JSON файл с временной меткой."""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    file_path_with_timestamp = os.path.join(directory, f"{base_filename}_diff_{timestamp}.json")
+    with open(file_path_with_timestamp, 'w', encoding='utf-8') as file:
+        json.dump(diff, file, indent=4, default=str)
 
 
 def get_latest_spec_in_range(directory, base_filename, start_time=None, end_time=None):
     """Получает последнюю спецификацию в указанном диапазоне времени или самую свежую."""
     latest_spec = None
     latest_time = None
+
+    if not os.path.exists(directory):
+        return None
 
     for filename in os.listdir(directory):
         if filename.startswith(base_filename) and filename.endswith('.json'):
@@ -68,44 +91,66 @@ def compare_specs(current_spec, previous_spec):
     return diff
 
 
-def main(current_spec_path, directory, base_filename, start_time=None, end_time=None):
-    """Основная функция для загрузки, сравнения и сохранения спецификаций."""
-    # Загрузить текущую спецификацию
-    current_spec = load_spec(current_spec_path)
+def extract_default_directory(url):
+    """Извлекает третий уровень с конца в пути URL."""
+    path_parts = urlparse(url).path.split('/')
+    if len(path_parts) >= 2:
+        return path_parts[-2]
+    return '.'
+
+
+def process_url(current_spec_url, base_filename, work_dir, start_time=None, end_time=None):
+    """Процесс сравнения для одного URL."""
+    default_directory = extract_default_directory(current_spec_url)
+
+    current_spec_directory = os.path.join(work_dir, cur_spec, default_directory)
+    diff_directory = os.path.join(work_dir, diff, default_directory)
+
+    # Устанавливаем префикс файла по умолчанию, если он не указан
+    if base_filename is None:
+        base_filename = default_directory
+
+    current_spec = load_spec(current_spec_url)
     if not current_spec:
-        print("Текущая спецификация невалидна. Завершение работы.")
+        print(f"Текущая спецификация по URL {current_spec_url} невалидна. Пропуск.")
         return
 
-    # Определить последнюю предыдущую спецификацию в указанном диапазоне времени
-    latest_spec_path = get_latest_spec_in_range(directory, base_filename, start_time, end_time)
+    latest_spec_path = get_latest_spec_in_range(current_spec_directory, base_filename, start_time, end_time)
     previous_spec = load_spec(latest_spec_path) if latest_spec_path else None
 
-    # Сравнить спецификации
     if previous_spec:
         changes = compare_specs(current_spec, previous_spec)
         if changes:
-            print("Найдены изменения в спецификации:")
-            print(
-                json.dumps(changes, indent=4, default=str))  # Преобразование изменений в строку для сериализации в JSON
+            print(f"Найдены изменения в спецификации по URL {current_spec_url}:")
+            save_diff(changes, diff_directory, base_filename)
         else:
-            print("Изменений в спецификации не найдено.")
+            print(f"Изменений в спецификации по URL {current_spec_url} не найдено.")
     else:
-        print("Предыдущая спецификация не найдена или невалидна. Создается новый файл.")
+        print(f"Предыдущая спецификация по URL {current_spec_url} не найдена или невалидна. Создается новый файл.")
 
-    # Сохранить текущую спецификацию как предыдущую для следующего сравнения
-    save_spec(current_spec, os.path.join(directory, base_filename + '.json'))
+    save_spec(current_spec, current_spec_directory, base_filename)
+
+
+def main(urls_file, work_dir='.', base_filename=None, start_time=None, end_time=None):
+    """Основная функция для загрузки, сравнения и сохранения спецификаций для нескольких URL."""
+    with open(urls_file, 'r', encoding='utf-8') as file:
+        urls = [line.strip() for line in file if line.strip()]
+
+    for url in urls:
+        process_url(url, base_filename, work_dir, start_time, end_time)
 
 
 if __name__ == "__main__":
-    current_spec_path = 'https://petstore.swagger.io/v2/swagger.json'  # URL или путь к текущей спецификации
-    directory = './specs'  # Директория для хранения спецификаций
-    base_filename = 'previous_spec'  # Базовое имя для хранения предыдущих спецификаций
+    urls_file = 'urls.txt'  # Файл с URL-адресами
 
     # Пример для использования абсолютного диапазона времени
-    start_time_str = '2024-05-01 00:00:00'  # Задайте начальную дату и время
-    end_time_str = '2024-05-30 00:00:00'  # Задайте конечную дату и время
+    start_time_str = '2023-05-01 00:00:00'  # Задайте начальную дату и время
+    end_time_str = '2023-06-01 00:00:00'  # Задайте конечную дату и время
 
     start_time = datetime.strptime(start_time_str, '%Y-%m-%d %H:%M:%S') if start_time_str else None
     end_time = datetime.strptime(end_time_str, '%Y-%m-%d %H:%M:%S') if end_time_str else None
 
-    main(current_spec_path, directory, base_filename, start_time, end_time)
+    # Указываем рабочую директорию (например, "C:\\results")
+    work_dir = "C:\\results"
+
+    main(urls_file, work_dir)
